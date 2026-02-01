928e18576d8c38b17e9030914793ab9415bea51c
spinline <bilal.karatatr.@gmail.com>
2026-02-01 18:20:12 +0300

    feat(frontend): enable all daisyui themes in configuration and UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,6 +14,6 @@ module.exports = {
   },
   plugins: [],
   daisyui: {
-    themes: ["dark", "light", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter", "dim", "nord", "sunset"],
   },
 }
